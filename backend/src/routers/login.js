19b204fff0d76715f97156fd962bf4ae75f81244
Oscar Rauda <20220631@ricaldone.edu.sv>
2025-04-29 19:11:44 -0600

    Mas comentarios

--- backend/src/routers/login.js
+++ backend/src/routers/login.js
@@ -1,8 +1,12 @@
-import express from "express"
+import express from "express";
 import loginController from "../controllers/loginController.js";
 
+// Crear el router para manejar la ruta de inicio de sesión
 const router = express.Router();
 
-router.route("/").post(loginController.login)
+// Ruta para login: procesa la solicitud de inicio de sesión
+router.route("/")
+    .post(loginController.login); // Iniciar sesión con los datos del usuario
 
+// Exportación del router para usarlo en la app principal
 export default router;
